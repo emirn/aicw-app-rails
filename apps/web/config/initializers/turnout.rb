6cfb38f2e6b19b6f50b80c98fb5c3c71e19d19fb
E <emirn@users.noreply.github.com>
2026-02-10 00:07:53 +0400

    Add Turnout gem for maintenance mode (#36)

---
+++ apps/web/config/initializers/turnout.rb
@@ -0,0 +1,6 @@
+Turnout.configure do |config|
+  config.named_maintenance_file_paths = { default: Rails.root.join("storage", "maintenance.yml") }
+  config.default_maintenance_page = Turnout::MaintenancePage::HTML
+  config.default_reason = "We are performing scheduled maintenance. Please check back soon."
+  config.default_allowed_paths = ["/up"]
+end
